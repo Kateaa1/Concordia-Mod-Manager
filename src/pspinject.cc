8f56392683c702f6c76dad2d25704ca40c5c455e
Kate <katelyn.r.sellars@proton.me>
2026-03-19 22:25:39 +0800

    made psp extract use filesystem::copy instead of cp

--- src/pspinject.cc
+++ src/pspinject.cc
@@ -16,15 +16,12 @@ void psp_inject(fs::path& source_dir, fs::path& target_dir, std::string modtype)
         source_dir = source_dir / "SYSDIR" / "EBOOT.BIN";
         fs::copy_file(source_dir, output_dir, fs::copy_options::overwrite_existing);
     } else if (modtype == "sound") {
-            fs::path output_dir = target_dir / "PSP_GAME" / "USRDIR";
+            fs::path output_dir = target_dir / "PSP_GAME" / "USRDIR" / "sdata" /"";
             source_dir = source_dir / "USRDIR" / "sdata/";
-            std::string sound_install =
-                "cp -r \"" + source_dir.string() + "\" \"" + output_dir.string() +"\"";
-           int result = std::system(sound_install.c_str());
-           if (result != 0){
-               std::cout << "error\n"; }
-           std::cout << "mods loaded from: " << source_dir << "\n";
-           std::cout << "mods installed to: " << output_dir << "\n";
+            fs::copy(source_dir, output_dir, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
+            std::cout << "copied files using fs::copy\n";         
+            std::cout << "mods loaded from: " << source_dir << "\n";
+             std::cout << "mods installed to: " << output_dir << "\n";
        } else {
         std::cout << "error, kate is dumb\n";
     }
